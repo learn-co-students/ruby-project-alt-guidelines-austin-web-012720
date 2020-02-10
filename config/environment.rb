b536aa1e042c7c878cdbc81b7c3943d9eb0933e9
jbshipman <jbshipman@icloud.com>
2020-02-10 14:55:11 -0600

    reformatted environment file, just to match lab files

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,8 @@ Bundler.require
 require 'active_record'
 require_all 'app/models'
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.establish_connection (
+  adapter: 'sqlite3', 
+  database: 'db/development.db'
+  )
 require_all 'lib'
