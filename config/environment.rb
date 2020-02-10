8e1f0686ae707483653f4e94cd3db07093074065
jbshipman <jbshipman@icloud.com>
2020-02-10 15:37:41 -0600

    file updates

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ Bundler.require
 require 'active_record'
 require_all 'app/models'
 
-ActiveRecord::Base.establish_connection (
+ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3', 
   database: 'db/development.db'
   )
