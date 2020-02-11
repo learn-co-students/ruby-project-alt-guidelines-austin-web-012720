d8b8056a6449afbecdebbbc7b7b997588eb7fdbc
github username <github email address>
2020-02-11 11:32:46 -0600

    Added prompts to Veg and Prot, added header.

--- app/models/protein.rb
+++ app/models/protein.rb
@@ -1,4 +1,23 @@
+require "tty-prompt"
+require 'pry'
+
 class Protein < ActiveRecord::Base
   has_many :recipes
   has_many :vegetables, through: :recipes
+  attr_accessor :protein_choice
+
+  
+  def self.protein_select
+    prompt = TTY::Prompt.new
+    @protein_choice = prompt.select("Please pick your Protein", %w(Chicken Beef Pork Tofu))
+      until prompt.yes?("You selected #{@protein_choice}, is this correct?") == true
+        @protein_choice = prompt.select("Pick your protein carefully this time...", %w(Chicken Beef Pork Tofu))
+      end
+    return @protein_choice 
+  end
+
+  def self.protein_choice
+    @protein_choice
+  end
+
 end
