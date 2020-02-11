f0b79857f3724ab86a2ead0d01fbf3448d08d66e
jbshipman <jbshipman@icloud.com>
2020-02-11 15:32:15 -0600

    updated protein model for merge

--- app/models/protein.rb
+++ app/models/protein.rb
@@ -6,12 +6,15 @@ class Protein < ActiveRecord::Base
   has_many :vegetables, through: :recipes
   attr_accessor :protein_choice
 
+  def self.list_all_protein_names
+    Protein.all.map { |proteins| proteins.name}
+  end 
   
   def self.protein_select
     prompt = TTY::Prompt.new
-    @protein_choice = prompt.select("Please pick your Protein", %w(Chicken Beef Pork Tofu))
+    @protein_choice = prompt.select("Please pick your Protein", list_all_protein_names)
       until prompt.yes?("You selected #{@protein_choice}, is this correct?") == true
-        @protein_choice = prompt.select("Pick your protein carefully this time...", %w(Chicken Beef Pork Tofu))
+        @protein_choice = prompt.select("Pick your protein carefully this time...", list_all_protein_names)
       end
     return @protein_choice 
   end
