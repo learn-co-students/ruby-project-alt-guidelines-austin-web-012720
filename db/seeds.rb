be1e4bdf50ca4b19b38ffd3bbe57b11f6b2a6c64
jbshipman <jbshipman@icloud.com>
2020-02-11 15:32:32 -0600

    seeds file to merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,16 @@ Protein.create(:name => "Chicken")
 Protein.create(:name => "Pork")
 Protein.create(:name => "Beef")
 Protein.create(:name => "Fish")
+Protein.create(:name => "Turduckin")
+Protein.create(:name => "Bison")
+
 
 Vegetable.create(:name => "Carrot")
 Vegetable.create(:name => "Potato")
 Vegetable.create(:name => "Leak")
 Vegetable.create(:name => "Onion")
+Vegetable.create(:name => "Corn")
 
-Recipe.create(:name => "recipe 1", :vegetable_id => 1, :protein_id => 1)
-Recipe.create(:name => "recipe 2", :vegetable_id => 2, :protein_id => 1)
+# remove these from being used //james
+# Recipe.create(:name => "recipe 1", :vegetable_id => 1, :protein_id => 1)
+# Recipe.create(:name => "recipe 2", :vegetable_id => 2, :protein_id => 1)
