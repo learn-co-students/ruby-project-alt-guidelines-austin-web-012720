15900703c803624d1d26f49d6071a21ff184decd
jbshipman <jbshipman@icloud.com>
2020-02-12 15:41:11 -0600

    regex commands tested

--- app/models/API.rb
+++ app/models/API.rb
@@ -11,11 +11,47 @@ class Api < ActiveRecord::Base
         recipe = response.body
         recipe_hash = JSON.parse(recipe)
 
+        # method to format the instructions
+        
+
         puts recipe_hash["recipes"][0]["title"]
         puts
         puts recipe_hash["recipes"][0]["instructions"]
+        # text_clean_up(recipe_hash["recipes"][0]["instructions"])
+        # recipe_hash["recipes"][0]["instructions"].text_clean_up
         puts
 
     end
 
+    # def text_clean_up(text)
+    #     # text = recipe_hash["recipes"][0]["instructions"]
+    #     i = 1
+    #     # no change needed
+    #     puts "#{text}" if text.scan(/^.*/)
+
+    #     # plain lump return
+    #     if text.scan(/$/)
+    #         while i < text.scan(/$/).count do
+    #             puts "#{i}. #{text.split(/$/)[i]}"
+    #             i += 1
+    #         end 
+    #     end 
+
+    #     # ordered list lump
+    #     if text.scan(/\d\./)
+    #         while i < text.scan(/\d\./).count do
+    #             puts "#{i}. #{text.split(/\d\./)[i]}"
+    #             i += 1
+    #         end
+    #     end 
+
+    #     # remove html tags
+    #     if text.scan(/<ol>/)
+    #     text_array = text.gsub(/<\/li>|<ol>|<\/ol>|<\/html>|<\/body>/,'').split(/<li>/)
+    #         while i < text_array.length do
+    #             puts "#{i}. #{text_array[i]}"
+    #             i += 1
+    #         end 
+    #     end
+    # end 
 end
