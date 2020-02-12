d9424a13d861a67bd794229e8923e6264ab3d807
jbshipman <jbshipman@icloud.com>
2020-02-12 16:35:38 -0600

    clean up of regex groups

--- app/models/API.rb
+++ app/models/API.rb
@@ -10,48 +10,37 @@ class Api < ActiveRecord::Base
         response = Net::HTTP.get_response(uri)
         recipe = response.body
         recipe_hash = JSON.parse(recipe)
-
-        # method to format the instructions
         
-
         puts recipe_hash["recipes"][0]["title"]
         puts
-        puts recipe_hash["recipes"][0]["instructions"]
-        # text_clean_up(recipe_hash["recipes"][0]["instructions"])
-        # recipe_hash["recipes"][0]["instructions"].text_clean_up
-        puts
 
+        # puts recipe instructions with basic formatting
+        text = recipe_hash["recipes"][0]["instructions"]
+        i = 1
+
+        # newline used lump return
+        if text.scan(/$/)
+            while i < text.scan(/$/).count do
+                puts "#{i}. #{text.split(/$/)[i]}"
+                i += 1
+            end 
+        end 
+
+        # ordered list lump
+        if text.scan(/\d\./)
+            while i < text.scan(/\d\./).count do
+                puts "#{i}. #{text.split(/\d\./)[i]}"
+                i += 1
+            end
+        end 
+
+        # remove html tags
+        if text.scan(/<ol>/)
+        text_array = text.gsub(/<\/li>|<ol>|<\/ol>|<\/html>|<\/body>/,'').split(/<li>/)
+            while i < text_array.length do
+                puts "#{i}. #{text_array[i]}"
+                i += 1
+            end 
+        end
     end
-
-    # def text_clean_up(text)
-    #     # text = recipe_hash["recipes"][0]["instructions"]
-    #     i = 1
-    #     # no change needed
-    #     puts "#{text}" if text.scan(/^.*/)
-
-    #     # plain lump return
-    #     if text.scan(/$/)
-    #         while i < text.scan(/$/).count do
-    #             puts "#{i}. #{text.split(/$/)[i]}"
-    #             i += 1
-    #         end 
-    #     end 
-
-    #     # ordered list lump
-    #     if text.scan(/\d\./)
-    #         while i < text.scan(/\d\./).count do
-    #             puts "#{i}. #{text.split(/\d\./)[i]}"
-    #             i += 1
-    #         end
-    #     end 
-
-    #     # remove html tags
-    #     if text.scan(/<ol>/)
-    #     text_array = text.gsub(/<\/li>|<ol>|<\/ol>|<\/html>|<\/body>/,'').split(/<li>/)
-    #         while i < text_array.length do
-    #             puts "#{i}. #{text_array[i]}"
-    #             i += 1
-    #         end 
-    #     end
-    # end 
 end
